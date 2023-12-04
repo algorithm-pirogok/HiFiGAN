211bb8989677b25c284b35aee03f2ba130be1689
Oleg <oleg.kurilov@raiffeisen.ru>
2023-12-04 22:39:03 +0300

    add test

--- test.py
+++ test.py
@@ -9,12 +9,14 @@ import numpy as np
 import hydra
 from hydra.utils import instantiate
 import torch
+import torchaudio
 from tqdm import tqdm
 import pyloudnorm as pyln
 
 import tts.model as module_model
+from tts.datasets.MelSpectrogram import MelSpectrogram
 from tts.trainer import Trainer
-from tts.utils import ROOT_PATH, get_logger
+from tts.utils import ROOT_PATH, MelSpectrogramConfig
 from tts.utils.object_loading import get_dataloaders
 
 DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
@@ -23,14 +25,9 @@ DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
 @hydra.main(config_path='tts/configs', config_name='test_config')
 def main(clf):
 
-
     # define cpu or gpu if possible
     device = "cpu"
 
-    # setup data_loader instances
-    # dataloaders = get_dataloaders(clf)
-
-    # build model architecture
     model = instantiate(clf["arch"])
     checkpoint = torch.load(clf.checkpoint, map_location=device)
     state_dict = checkpoint["state_dict"]
@@ -41,60 +38,19 @@ def main(clf):
     # prepare model for testing
     model = model.to(device)
     model.eval()
-    exit()
-    results = []
-    metrcics = defaultdict(list)
-
-
-    norm_wav = pyln.Meter(clf["preprocessing"]["sr"])
 
     with torch.no_grad():
-        for dataset in dataloaders.keys():
-            for batch_num, batch in enumerate(tqdm(dataloaders[dataset])):
-                batch = Trainer.move_batch_to_device(batch, device)
-                output = model(**batch)
-                if type(output) is dict:
-                    batch.update(output)
-                else:
-                    raise Exception("change type of model")
-                for ind in range(len(batch['snr'])):
-                    for mode in ('short', 'middle', 'long'):
-                        batch[mode][ind] = torch.tensor(pyln.normalize.loudness(
-                            batch[mode][ind].cpu().numpy(),
-                            norm_wav.integrated_loudness(batch[mode][ind].cpu().numpy()),
-                            -23.0
-                        )).to(device)
-                    curr_batch = {key: batch[key][ind] for key in batch.keys()}
-                    metrcics["si_sdr"].append(si_sdr(**curr_batch).item())
-                    metrcics["comb_si_sdr"].append(comb_si_sdr(**curr_batch).item())
-                    metrcics["pesq"].append(pesq(**curr_batch).item())
-                    metrcics["comb_pesq"].append(comb_pesq(**curr_batch).item())
-
-                print("Iteration:", batch_num)
-                for key, value in metrcics.items():
-                    print(f"{key}: {np.mean(value)}")
-
-    final_dict = {}
-    for key, value in metrcics.items():
-        final_dict[key] = np.mean(value)
-        print(f"{key}: {np.mean(value)}")
-
-    with open(clf.out_file, "w") as f:
-        json.dump(final_dict, f, indent=2)
-
-    '''
-            logger.info(f"butch_num {batch_num}, len_of_object {len(metrcics['text_argmax'])}")
-
-            for key, history in metrcics.items():
-                wer, cer = zip(*history)
-                wer = np.mean(wer)
-                cer = np.mean(cer)
-                logger.info(f'{mode} {key}_WER = {wer}')
-                logger.info(f'{mode} {key}_CER = {cer}')
-
-            with Path(out_file).open("w") as f:
-                json.dump(models, f, indent=2)'''
-
+        config = MelSpectrogramConfig()
+        wav_to_mel = MelSpectrogram(config)
+        directory = ROOT_PATH / "data" / "test"
+        directory_save = ROOT_PATH / "data" / "predictions"
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory, filename)
+            if os.path.isfile(filepath):
+                target_audio, sample_rate = torchaudio.load(filepath)
+                target_mel = wav_to_mel(target_audio)
+                pred_audio = model.generator(target_mel).squeeze(0)
+                torchaudio.save(directory_save / filename, pred_audio, sample_rate)
 
 if __name__ == "__main__":
     main()
